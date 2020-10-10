48678dc2f60046c0004ae04f95b049ada581ad11
Valeriy-Leonov <valeoprint@ukr.net>
2020-10-10 17:01:44 +0300

    Dom 5

--- task-5/task-5.js
+++ task-5/task-5.js
@@ -2,8 +2,6 @@ const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', event => {
-  console.log('event.target', event.target.value);
-  console.log('inputEl.value', inputEl.value);
   const curentValue = inputEl.value;
   outputEl.innerText = curentValue || 'незнакомец';
 });
