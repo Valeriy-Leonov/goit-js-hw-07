7229e0f36021f57a960175d1ca3762e4304e4321
Valeriy-Leonov <valeoprint@ukr.net>
2020-10-10 17:34:12 +0300

    Dom 6

--- task-6/task-6.js
+++ task-6/task-6.js
@@ -1,20 +1,15 @@
-// Write code under this line
-const getUsersWithAge = (array, min, max) =>
-  array
-    .filter(({ age }) => min < age && age < max)
-    .map(({ name, email }) => ({ name, email }));
+const inputEl = document.querySelector('#validation-input');
+const neededLength = Number(inputEl.dataset.length);
 
-console.log(getUsersWithAge(users, 20, 30));
-/* [
-    { name: 'Ross Vazquez', email: 'rossvazquez@xinware.com' },
-    { name: 'Elma Head', email: 'elmahead@omatom.com' },
-    { name: 'Carey Barr', email: 'careybarr@nurali.com' }
-] */
+inputEl.addEventListener('blur', event => {
+  const currentValue = event.target.value.length;
+  if (currentValue === neededLength) {
+    inputEl.classList.add('valid');
+  } else {
+    inputEl.classList.add('invalid');
+  }
+});
 
-// console.log(getUsersWithAge(users, 30, 40));
-/* [
-    { name: 'Moore Hensley', email: 'moorehensley@indexia.com' },
-    { name: 'Sharlene Bush', email: 'sharlenebush@tubesys.com' },
-    { name: 'Blackburn Dotson', email: 'blackburndotson@furnigeer.com' },
-    { name: 'Sheree Anthony', email: 'shereeanthony@kog.com' }
-] */
+inputEl.addEventListener('focus', event => {
+  inputEl.classList = '';
+});
