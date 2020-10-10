fe73243715c761b37776741ec9406306678841a1
Valeriy-Leonov <valeoprint@ukr.net>
2020-10-10 16:34:59 +0300

    Dom 4

--- task-4/task-4.js
+++ task-4/task-4.js
@@ -1,47 +1,13 @@
-// Write code under this line
-const getInactiveUsers = array =>
-  array.filter(object => {
-    const { isActive } = object;
-    return !isActive;
-  });
+const decrementEl = document.querySelector('[data-action="decrement"]');
+const incrementEl = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
 
-// console.log(getInactiveUsers(users));
+decrementEl.addEventListener('click', event => {
+  let curentValue = Number(valueEl.innerText);
+  valueEl.innerText = --curentValue;
+});
 
-/* [
-  {
-    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
-    name: 'Moore Hensley',
-    email: 'moorehensley@indexia.com',
-    eyeColor: 'blue',
-    friends: ['Sharron Pace'],
-    isActive: false,
-    balance: 2811,
-    skills: ['ipsum', 'lorem'],
-    gender: 'male',
-    age: 37,
-  },
-  {
-    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
-    name: 'Ross Vazquez',
-    email: 'rossvazquez@xinware.com',
-    eyeColor: 'green',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-    isActive: false,
-    balance: 3793,
-    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-    gender: 'male',
-    age: 24,
-  },
-  {
-    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
-    name: 'Blackburn Dotson',
-    email: 'blackburndotson@furnigeer.com',
-    eyeColor: 'brown',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-    isActive: false,
-    balance: 1498,
-    skills: ['non', 'amet', 'ipsum'],
-    gender: 'male',
-    age: 38,
-  }
-]; */
+incrementEl.addEventListener('click', event => {
+  let curentValue = Number(valueEl.innerText);
+  valueEl.innerText = ++curentValue;
+});
