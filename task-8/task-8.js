9ab753366f540ea3ac078524e28fc89ddc675507
Valeriy-Leonov <valeoprint@ukr.net>
2020-10-17 19:09:11 +0300

    Dom-8

--- task-8/task-8.js
+++ task-8/task-8.js
@@ -1,11 +1,41 @@
-// Write code under this line
-const getUsersWithFriend = (array, friendName) =>
-  array
-    .filter(({ friends }) => friends.includes(friendName))
-    .map(({ name }) => name);
+const numberEl = document.querySelector('input');
+const createBtn = document.querySelector('[data-action="render"]');
+const clearBtn = document.querySelector('[data-action="destroy"]');
+const boxEl = document.querySelector('#boxes');
 
-// console.log(getUsersWithFriend(users, 'Briana Decker'));
-// [ 'Sharlene Bush', 'Sheree Anthony' ]
+function createBoxes(amount) {
+  let curentPx = 30;
+  const stepPx = 10;
+  const result = [];
 
-// console.log(getUsersWithFriend(users, 'Goldie Gentry'));
-// [ 'Elma Head', 'Sheree Anthony' ]
+  for (let i = 0; i < amount; i++) {
+    const element = document.createElement('div');
+    element.innerText = i;
+    element.style.backgroundColor = `rgb(${randomNumber(0, 255)},${randomNumber(
+      0,
+      255,
+    )},${randomNumber(0, 255)})`;
+    element.style.width = `${curentPx}px`;
+    element.style.height = `${curentPx}px`;
+    curentPx += stepPx;
+    result.push(element);
+  }
+  return result;
+}
+
+function randomNumber(minimum, maximum) {
+  return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+}
+
+function clearElementChild(el) {
+  el.innerHTML = '';
+}
+
+createBtn.addEventListener('click', event => {
+  clearElementChild(boxEl);
+  boxEl.append(...createBoxes(numberEl.value));
+});
+
+clearBtn.addEventListener('click', event => {
+  clearElementChild(boxEl);
+});
